230b7af2302ef4694d14b80955d40c9c23c56329
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-05-15 11:07:13 -0400

    Add files via upload

---
+++ secure-api-endpoint/src/_helpers/backend.js
@@ -0,0 +1,46 @@
+export function backend() {
+    let users = [{ id: 1, username: 'demo', password: 'turnerhacks', firstName: 'Test User', lastName: 'Demo' }];
+    let realFetch = window.fetch;
+    window.fetch = function (url, opts) {
+        const isLoggedIn = opts.headers['Authorization'] === 'Bearer fake-jwt-token';
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (url.endsWith('/users/authenticate') && opts.method === 'POST') {
+                    const params = JSON.parse(opts.body);
+                    const user = users.find(x => x.username === params.username && x.password === params.password);
+                    if (!user) return error('Username or password is incorrect');
+                    return ok({
+                        id: user.id,
+                        username: user.username,
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                        token: 'fake-jwt-token'
+                    });
+                }
+
+                if (url.endsWith('/users') && opts.method === 'GET') {
+                    if (!isLoggedIn) return unauthorised();
+                    return ok(users);
+                }
+
+                // pass through any requests not handled above
+                realFetch(url, opts).then(response => resolve(response));
+
+                // private helper functions
+
+                function ok(body) {
+                    resolve({ ok: true, text: () => Promise.resolve(JSON.stringify(body)) })
+                }
+
+                function unauthorised() {
+                    resolve({ status: 401, text: () => Promise.resolve(JSON.stringify({ message: 'Unauthorised' })) })
+                }
+
+                function error(message) {
+                    resolve({ status: 400, text: () => Promise.resolve(JSON.stringify({ message })) })
+                }
+            }, 500);
+        });
+    }
+}
---
+++ secure-api-endpoint/src/_helpers/index.js
@@ -0,0 +1,4 @@
+export * from './auth-header';
+export * from './backend';
+export * from './handle-response';
+export * from './history';
