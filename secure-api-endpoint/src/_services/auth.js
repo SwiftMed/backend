06f8a4071bfeb4539763a3e1ab94e82abd0dc407
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-05-15 11:16:27 -0400

    Update and rename authentication.service.js to auth.js

--- secure-api-endpoint/src/_services/auth.js
+++ secure-api-endpoint/src/_services/auth.js
@@ -22,7 +22,6 @@ function login(username, password) {
     return fetch(`${config.apiUrl}/users/authenticate`, requestOptions)
         .then(handleResponse)
         .then(user => {
-            // store user details and jwt token in local storage to keep user logged in between page refreshes
             localStorage.setItem('currentUser', JSON.stringify(user));
             currentUserSubject.next(user);
 
@@ -31,7 +30,6 @@ function login(username, password) {
 }
 
 function logout() {
-    // remove user from local storage to log user out
     localStorage.removeItem('currentUser');
     currentUserSubject.next(null);
 }
