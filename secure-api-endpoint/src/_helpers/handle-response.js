371d8450280a968d9ce6f2cba6795c4e9b4a3abc
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-05-15 11:15:17 -0400

    Update handle-response.js

--- secure-api-endpoint/src/_helpers/handle-response.js
+++ secure-api-endpoint/src/_helpers/handle-response.js
@@ -5,7 +5,6 @@ export function handleResponse(response) {
         const data = text && JSON.parse(text);
         if (!response.ok) {
             if ([401, 403].indexOf(response.status) !== -1) {
-                // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
                 authenticationService.logout();
                 location.reload(true);
             }
