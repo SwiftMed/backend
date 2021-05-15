9b02615d70d677fbb9eed7025ae20df29e501fab
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-05-15 11:15:05 -0400

    Update PrivateRoute.jsx

--- secure-api-endpoint/src/_components/PrivateRoute.jsx
+++ secure-api-endpoint/src/_components/PrivateRoute.jsx
@@ -7,11 +7,9 @@ export const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route {...rest} render={props => {
         const currentUser = authenticationService.currentUserValue;
         if (!currentUser) {
-            // not logged in so redirect to login page with the return url
             return <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
         }
 
-        // authorised so return component
-        return <Component {...props} />
+        return <Component {props} />
     }} />
 )
